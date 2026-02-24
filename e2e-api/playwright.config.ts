d2d9cab9e215735f0b69037fb5e0dfc27386d682
Tor Thanawat <bunysiththiphic@gmail.com>
2026-02-24 14:23:11 +0700

    [Fixed]: แก้ไข config ใส่ comment ไม่ครบ ใน file playwright.config.ts

--- e2e-api/playwright.config.ts
+++ e2e-api/playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
+    // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
     // },
